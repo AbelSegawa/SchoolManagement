a1ffbfb91e9fb41f9c806e1fcb852cba24bb428a
AbelSegawa <abelsegawa9@gmail.com>
2026-06-25 23:52:08 +0300

    Add Flask web version

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
-from kivymd.app import MDApp
-from kivy.uix.screenmanager import ScreenManager
-from screens.login import LoginScreen
-from screens.dashboard import DashboardScreen
-from database.db import init_db
+from flask import Flask, render_template, request, redirect, url_for
+from database.db import init_db, get_conn
 
-class SchoolApp(MDApp):
-    def build(self):
-        init_db()
-        sm = ScreenManager()
-        sm.add_widget(LoginScreen(name='login'))
-        sm.add_widget(DashboardScreen(name='dashboard'))
-        return sm
+app = Flask(__name__)
 
-SchoolApp().run()
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/dashboard', methods=['POST'])
+def dashboard():
+    username = request.form['username']
+    password = request.form['password']
+    # TODO: check user in DB
+    return render_template('dashboard.html', user=username)
+
+if __name__ == '__main__':
+    init_db()
+    app.run(host='0.0.0.0', port=5000)
